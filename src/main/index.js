0f3e3a43e22a1f1beb16a2fb4231beb8706f12a3
nobergan <68248853+Nobergan@users.noreply.github.com>
2022-03-14 20:15:13 +0200

    done

---
+++ src/main/index.js
@@ -0,0 +1,14 @@
+import "./scss/index.scss";
+import "./js"
+
+import { CountUp } from 'countup.js';
+
+const options = {
+  startVal: 1543327,
+    duration: 100000,
+  prefix: '$',
+}
+window.onload = function () {
+    var countUp = new CountUp('count-num', 99000000, options);
+    countUp.start();
+}
